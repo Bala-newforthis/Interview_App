8c3c428d52be57b9ee99db881e86b5791cc1dae8
Balaji Reddy <balajireddy.m6@gmail.com>
2026-04-28 20:38:46 +0530

    we have used the jsdocs comment for better understanding

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,7 @@
 // api creation in this file for my references
 
 const { Router } = require("express");  // const express = require("express")
-const authRouter = require("../controller/auth.controller")
+const authController = require ("../controller/auth.controller");
 
 const authRouter = Router();  // const authRouter = express.Router();
 
@@ -12,6 +12,14 @@ const authRouter = Router();  // const authRouter = express.Router();
  * @access Public
  */
 
-authRouter.post ("/register", authController.registerUserController)
+authRouter.post ("/register", authController.registerUserController);
+
+/**
+ * @route POST /api/auth/login
+ * @description  Login a user with email and password
+ * @access Public
+ */
+
+authRouter.post("/login",authController.loginUserController);
 
 module.exports = authRouter;
