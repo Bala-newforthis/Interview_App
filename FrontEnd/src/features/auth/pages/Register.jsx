e9fa285d32bd8b23691498a37814ea9911d5e11e
Balaji Reddy <balajireddy.m6@gmail.com>
2026-05-01 23:59:27 +0530

    created a register page and done with basic styling in that ui

--- FrontEnd/src/features/auth/pages/Register.jsx
+++ FrontEnd/src/features/auth/pages/Register.jsx
@@ -1,9 +1,32 @@
 import React from "react";
 
 const Register = () => {
+
+    const handlesubmit = (e) => {
+        e.preventDefault()
+    }
+
     return (
         <main>
+
             
+            <div className="form-container">
+                <h1>Register</h1>
+
+                    <form onSubmit={handlesubmit}>
+                <div className="input-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="email" placeholder="Enter your email address"  />
+                </div>
+
+                <div className="input-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" placeholder="Enter your password" />
+                </div>
+
+                <button className="button primary-button"><b>Register</b></button>
+            </form>
+            </div>
         </main>
     )
 }
