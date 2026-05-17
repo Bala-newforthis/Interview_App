8b4a7ac4ae206315adefd6f8b430fd86d5953fb2
Balaji Reddy <balajireddy.m6@gmail.com>
2026-05-17 22:04:25 +0530

    updated the folder in this interview.routes.js

--- BackEnd/src/routes/interview.routes.js
+++ BackEnd/src/routes/interview.routes.js
@@ -6,7 +6,7 @@ const interviewRouter = express.Router()
 
 /**
  * @route POST /api/interview/
- * @description generate new interview report on the basic of user self description , resume pdf and job description 
+ * @description generate new interview report on the basis of user self description , resume pdf and job description .
  * @access private
  */
 interviewRouter.post("/", authMiddleware.authUser , interviewController.generateInterviewReportController)
